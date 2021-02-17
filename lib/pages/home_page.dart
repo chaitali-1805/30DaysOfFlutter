68edf2104d295b894e4d7b8cb3bc347920cff870
Chaitali Tamboliya <chaitalitamboliya@gmail.com>
2021-02-18 01:13:54 +0530

    Day12 - ListView, Card, Asserts

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catlog/models/catlog.dart';
 import 'package:flutter_catlog/widgets/drawer.dart';
+import 'package:flutter_catlog/widgets/item_widget.dart';
 
 // Day 11 context tree learning
 
@@ -9,13 +11,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatlogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catlog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcometo $days days of Flutter $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
