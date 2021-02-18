871ab4bd808d38ed3ac562d68b5b0c7e79d0812f
Chaitali Tamboliya <chaitalitamboliya@gmail.com>
2021-02-19 00:58:35 +0530

    Day13 - Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catlog/models/catlog.dart';
 import 'package:flutter_catlog/widgets/drawer.dart';
 import 'package:flutter_catlog/widgets/item_widget.dart';
-
+import 'dart:convert';
 // Day 11 context tree learning
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Chaitali";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catlogJson = await rootBundle.loadString("assets/files/catlog.json");
+    final decodedData = jsonDecode(catlogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
