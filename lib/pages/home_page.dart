5365cc6bd70550fe69a68efa8a40097ae9969dbd
Chaitali Tamboliya <chaitalitamboliya@gmail.com>
2021-02-20 00:14:47 +0530

    Day-16 Grid View

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_catlog/models/catlog.dart';
 import 'package:flutter_catlog/widgets/drawer.dart';
-import 'package:flutter_catlog/widgets/item_widget.dart';
+// import 'package:flutter_catlog/widgets/item_widget.dart';
 import 'dart:convert';
 // Day 11 context tree learning
 
@@ -43,12 +43,43 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatlogModel.items != null && CatlogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatlogModel.items.length,
-                itemBuilder: (context, index) =>  ItemWidget(
-                    item: CatlogModel.items[index],
-                  
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatlogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        child: Image.network(item.image),
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ));
                 },
+                itemCount: CatlogModel.items.length,
               )
             : Center(
                 child: CircularProgressIndicator(),
