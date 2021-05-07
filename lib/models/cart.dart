e2a20c621612f6678769e75fbf5af891aff1fdb6
Chaitali Tamboliya <chaitalitamboliya@gmail.com>
2021-05-07 15:24:04 +0530

    Day-27 VelocityX State Management

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_catlog/core/store.dart';
 import 'package:flutter_catlog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   // Catalog field
@@ -30,5 +32,15 @@ class CartModel {
   // Remove Item
   void remove(Item item) {
     _itemIds.remove(item.id);
+  }
+}
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+  @override
+  perform() {
+    store.cart._itemIds.add(item.id);
   }
 }
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catlog/core/store.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_catlog/models/cart.dart';
 
@@ -51,9 +52,9 @@ class _CartTotal extends StatelessWidget {
 }
 
 class _CartList extends StatelessWidget{
-  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
+    final CartModel _cart = (VxState.store as MyStore).cart;
     return _cart.items.isEmpty? "Noting to show".text.xl3.makeCentered() 
     : ListView.builder(
       itemCount: _cart.items?.length,
